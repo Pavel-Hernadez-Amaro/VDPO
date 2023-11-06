a3c68ecd7313d83ce852e814a252e5c5525e3152
nachodieez <nachodiez10@live.com>
2023-11-06 10:16:57 +0100

    Modified `VDFO.R`

--- R/VDFO.R
+++ R/VDFO.R
@@ -8,37 +8,38 @@ VDFO <- function(formula, data, family = stats::gaussian()) {
 
   tf <- terms.formula(formula, specials = c("ffvd"))
 
-  terms <- attr(tf, "term.labels")
-  nterms <- length(terms)
+  terms    <- attr(tf, "term.labels")
+  nterms   <- length(terms)
   specials <- attr(tf, "specials") # indices for the special terms
 
   if (attr(tf, "response")) {
     response <- as.character(attr(tf, "variables"))[2]
     variables <- as.character(attr(tf, "variables"))[-c(1,2)]
 
-    specials$ffvd <- specials$ffvd - 1
+    # If the response exists, we need to lower in the index for every
+    # special term
 
-    specials_indices <- c(specials$ffvd)
+    specials_indices <- vapply(specials,
+                               function(x) x - 1,
+                               FUN.VALUE = numeric(1))
   } else {
     variables <- as.character(attr(tf, "variables"))[-1]
+
+    # No need to lower the index of the specials terms if the response exists
+    specials_indices <- vapply(specials,
+                               function(x) x,
+                               FUN.VALUE = numeric(1))
   }
 
-  nfvariables <- variables[-specials_indices]
-  fvariables <- variables[specials_indices]
+  non_funcional_variables <- variables[-specials_indices]
+  functional_variables    <- variables[specials_indices]
 
-  fvariables_names <- lapply(fvariables,
-                             function (x) strsplit(
-                               strsplit(x, "\\(")[[1]][[2]],
-                               "\\)"
-                               )[[1]][[1]]
-  )
+  terms(formula)
+  vdfoenv <- environment(formula)
+  vdfons  <- loadNamespace("VDPO")
 
-  fvariables_functions <- lapply(fvariables,
-                                 function (x) strsplit(x, "\\(")[[1]][[1]])
+  eval(parse(terms[1]), envir = vdfons, enclos = vdfoenv)
 
-
-
-  # ALL THE COMPUTATION
 
 
 
@@ -48,26 +49,3 @@ VDFO <- function(formula, data, family = stats::gaussian()) {
 ffvd <- function(x) {
   ifelse(x<2, 10, 20)
 }
-
-f()
-
-formula <- x ~ a + b + ffvd(c)
-formula <- ~ a + b + ffvd(c)
-
-
-sop()
-
-m1 <- matrix(1:10,ncol=2)
-m2 <- matrix(5:14,ncol=2)
-dd <- list(m1,m2)
-
-m <- matrix(7:10, nrow = 2)
-
-data = data.frame(
-  a = 1:2,
-  b = 3:4,
-  y = 5:6
-)
-
-data[["c"]] <- m
-
