138c9db53669b6a9b3c101aae5917b6d52387aef
Pavel-Hernandez-Amaro <pahernan@est-econ.uc3m.es>
2024-12-15 23:41:40 +0100

    round 2

--- R/ffpo.R
+++ R/ffpo.R
@@ -58,7 +58,7 @@ ffpo <- function(X, grid, bidimensional_grid = FALSE, nbasis = c(30, 30), bdeg =
   c2 <- nbasis[2]
   A <- matrix(0, nrow = N, ncol = N * c1)
 
-  if (!is_grid_matrix) {
+  if (is_grid_matrix) {
     grid_all <- sort(unique(c(t(grid)))) # Mathematical union of the grid rows
     M <- t(apply(X, 1, function(x) range(which(!is.na(x)))))
   } else {
