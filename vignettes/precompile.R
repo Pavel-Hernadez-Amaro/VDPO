b8265a0b53e0884e1d094f76aa23c8f88d994a9e
Pavel Hernandez <pavel.amaro96@gmail.com>
2026-06-06 09:47:43 +0000

    Add partially observed and VD-MFPCA vignette sources

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,6 +4,8 @@ old_dir <- setwd("vignettes")
 # generate static Rmds
 knitr::knit("source/_VDPO-01-introduction.Rmd", "VDPO-01-introduction.Rmd")
 knitr::knit("source/_VDPO-02-vd-model.Rmd", "VDPO-02-vd-models.Rmd")
+knitr::knit("source/_VDPO-03-pofd.Rmd", "VDPO-03-pofd.Rmd")
+knitr::knit("source/_VDPO-04-vd-mfpca.Rmd", "VDPO-04-vd-mfpca.Rmd")
 
 # restore
 setwd(old_dir)
